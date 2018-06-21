d6ea78d55f59751312f0e303122442aa02fdaaa8
Mehdi ElHAIJ <mehdi.elhaij@hotmail.com>
2018-06-21 14:48:51 +0100

    run server

---
+++ app.js
@@ -0,0 +1,15 @@
+import { createServer } from './lib/server'
+import { logger } from './lib/logger'
+import { env } from './lib/env'
+
+createServer().then(
+  app =>
+    app.listen(env.PORT, () => {
+      const mode = env.NODE_ENV
+      logger.info(`Server listening on ${env.PORT} in ${mode} mode`)
+    }),
+  err => {
+    logger.error('Error while starting up server', err)
+    process.exit(1)
+  }
+)
